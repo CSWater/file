33d5ef24b806b435ffcdc97dc0f58a4e78628dc6
NcicWater <1219196322@qq.com>
2018-01-02 15:25:25 +0800

    random select optimization

--- TicketingDS.java
+++ TicketingDS.java
@@ -1,5 +1,6 @@
 package ticketingsystem;
 
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class TicketingDS implements TicketingSystem {
@@ -9,7 +10,9 @@ public class TicketingDS implements TicketingSystem {
 	public int stationnum;
 	public int threadnum;
 	public int totalseatnum;
+	public int sum_seats;
 	
+	Random rand;
 	//use to produce ticketid
 	public AtomicInteger tid = new AtomicInteger(0);
 	//trains
@@ -17,24 +20,27 @@ public class TicketingDS implements TicketingSystem {
 	
 
 	TicketingDS(){
+		rand = new Random();
 		this.routenum = 5;
 		this.coachnum = 8;
 		this.seatnum = 100;
 		this.stationnum = 10;
 		this.threadnum = 16;
+		sum_seats = coachnum * seatnum;
 		this.trains = new Train[routenum];
 		for (int i = 0;i < routenum;++i ) {
-			//System.out.println(totalseatnum);
 			trains[i] = new Train(coachnum, seatnum, stationnum);
 		}
 	}
 	
 	TicketingDS(int routenum,int coachnum,int seatnum,int stationnum,int threadnum){
+		rand = new Random();
 		this.routenum = routenum;
 		this.coachnum = coachnum;
 		this.seatnum = seatnum;
 		this.stationnum = stationnum;
 		this.threadnum = threadnum;
+		sum_seats = coachnum * seatnum;
 		this.trains = new Train[routenum];
 		for (int i = 0;i < routenum;++i ) {
 			trains[i] = new Train(coachnum, seatnum, stationnum);
@@ -43,13 +49,15 @@ public class TicketingDS implements TicketingSystem {
 	
 	public Ticket buyTicket(String passenger, int route, int departure, int arrival) {
 		Ticket ticket = new Ticket();
-		
 		//buy tickets in the route you want
 		Train train = trains[route - 1];
-		int[] ticketinfo = train.tryBuyTicket(departure, arrival);
+		//according to thread id to select a start point
+		//long idx = Thread.currentThread().getId();
+		//int start_point = ((int)idx & 0x40);				//select a start point to buy ticket
+		int start_point = rand.nextInt(sum_seats);
+		int[] ticketinfo = train.tryBuyTicket(start_point, departure, arrival);
 		if(ticketinfo != null) {
 			ticket.arrival = arrival;
-			
 			ticket.coach = ticketinfo[0];
 			ticket.departure = departure;
 			ticket.passenger = passenger;
--- Train.java
+++ Train.java
@@ -1,54 +1,81 @@
 package ticketingsystem;
 
+
 public class Train {
-	public int coachnum;
-	Coach[] coachs;
+	public int coach_size;			//how many seats per coach
+	public int coach_num;			//hou many coaches per train
+	public int seat_num;			//how many seats per train
+	Seat[] seats;
 	
-	public Train(int coachnum, int seatnum, int stationnum) {
-		coachs = new Coach[coachnum];
-		this.coachnum = coachnum;	
-		for(int i = 0;i < coachnum;++i) {
-			coachs[i] = new Coach(seatnum, stationnum);
+	public Train(int num_coach_per_train, int num_seat_per_coach, int num_station) {
+		coach_size = num_seat_per_coach;
+		coach_num = num_coach_per_train;
+		seat_num = coach_num * coach_size;
+		seats = new Seat[seat_num];	
+		for(int i = 0;i < seat_num; ++i) {
+			seats[i] = new Seat(num_station);
 		}
 	}
 	
-	public int[] tryBuyTicket(int departure, int arrival) {
+	public int[] tryBuyTicket(int start_point, int departure, int arrival) {
+		
 		int[] ticketinf = new int[2];
 		boolean flag1 = false;
 		boolean flag2 = false;
-		for(int coachindex = 0; coachindex < coachnum; ++coachindex) {
-			Coach coach = coachs[coachindex];
-			int seatnum = coach.seatnum;
-			for(int seatindex = 0; seatindex < seatnum; ++seatindex) {
-				Seat seat = coach.seats[seatindex];
-				flag1 = seat.validate(departure, arrival);
-				if(flag1) {
-					seat.seatlock.lock();
-					try {
-						flag2 = seat.validate(departure, arrival);
-						if(flag2) {
-							seat.lockSeat(departure, arrival);
-							ticketinf[1] = seatindex + 1;
-							break;
-						}
-					}finally {
-						seat.seatlock.unlock();
+		try {
+			for(int seat_index = start_point; seat_index < seat_num; ++seat_index) {
+			Seat seat = seats[seat_index];
+			flag1 = seat.validate(departure, arrival);
+			if(flag1) {
+				seat.seatlock.lock();
+				try {
+					flag2 = seat.validate(departure, arrival);
+					if(flag2) {
+						seat.lockSeat(departure, arrival);							
 					}
+				}finally {
+					seat.seatlock.unlock();
 				}
 			}
 			if(flag2) {
-				ticketinf[0] = coachindex + 1;
-				break;
+				ticketinf[0] = seat_index / coach_size + 1;
+				ticketinf[1] = seat_index % coach_size + 1;
+				return ticketinf;
 			}
-		}		
-		if(flag2)
-			return ticketinf;
-		else
+		}
+		} catch(Exception e) {
+			System.out.println(start_point);
+		}
+		flag1 = flag2 = false;
+		try {
+		for(int seat_index = 0; seat_index < start_point; ++seat_index) {
+			Seat seat = seats[seat_index];
+			flag1 = seat.validate(departure, arrival);
+			if(flag1) {
+				seat.seatlock.lock();
+				try {
+					flag2 = seat.validate(departure, arrival);
+					if(flag2) {
+						seat.lockSeat(departure, arrival);
+					}
+				}finally {
+					seat.seatlock.unlock();
+				}
+			}
+			if(flag2) {
+				ticketinf[0] = seat_index / coach_size + 1;
+				ticketinf[1] = seat_index % coach_size + 1;
+				return ticketinf;
+			}
+		}
+		} catch (Exception e) {
+			System.out.println(start_point);
+		}
 			return null;
 	}
 	
-	public boolean refundTicket(int coachindex,int seatindex,int departure,int arrival) {		
-		Seat seat = (coachs[coachindex].seats)[seatindex];
+	public boolean refundTicket(int coachindex,int seatindex,int departure,int arrival) {	
+		Seat seat = seats[coachindex * coach_size + seatindex];
 		seat.seatlock.lock();
 		try {
 			seat.unlockSeat(departure, arrival);
@@ -60,13 +87,9 @@ public class Train {
 		
 	public int inquiry(int departure, int arrival) {
 		int count = 0;
-		for(int coachindex = 0; coachindex < coachnum; ++coachindex) {
-			Coach coach = coachs[coachindex];
-			int seatnum = coach.seatnum;
-			for(int seatindex = 0; seatindex < seatnum; ++seatindex) {
-				Seat seat = coach.seats[seatindex];
-				if(seat.validate(departure, arrival))
-					++count;
+		for(int seat_index = 0; seat_index < seat_num; ++seat_index) {
+			if(seats[seat_index].validate(departure, arrival)) {
+				++count;
 			}
 		}
 		return count;
